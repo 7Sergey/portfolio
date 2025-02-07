40f6cd0f534f9eced0aee299d0493760b9f6b817
Sergey Fedorov <sergey14fedorov@gmail.com>
2025-02-07 12:55:18 +0300

    add card Email copied

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -4,6 +4,11 @@ import { cn } from "@/lib/utils";
 
 // import TailwindButton from "./TailwindButton";
 import { Cover } from "./Cover";
+import Lottie from "react-lottie";
+import { useState } from "react";
+import animationData from "@/data/confetti.json";
+import TailwindButton from "./TailwindButton";
+import { IoCopyOutline } from "react-icons/io5";
 
 export const BentoGrid = ({
   className,
@@ -44,6 +49,13 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText("sergey14fedorov@gmail.com");
+
+    setCopied(true);
+  };
   // const leftLists = ["React", "Redux", "Typescript", "TailWind"];
   // const rightLists = ["Node", "Express", "MongoDB"];
 
@@ -135,6 +147,30 @@ export const BentoGridItem = ({
                   </span>
                 ))}
               </div>
+            </div>
+          )}
+
+          {id === 6 && (
+            <div className="mt-5 relative">
+              <div className={`absolute -bottom-5 right-0`}>
+                <Lottie
+                  options={{
+                    loop: copied,
+                    autoplay: copied,
+                    animationData: animationData,
+                    rendererSettings: {
+                      preserveAspectRatio: "xMidYMid slice",
+                    },
+                  }}
+                />
+              </div>
+              <TailwindButton
+                title={copied ? "Email copied" : "Copy my email"}
+                icon={<IoCopyOutline />}
+                position="left"
+                otherClasses="!bg-[#161a31]"
+                handleClick={handleCopy}
+              />
             </div>
           )}
         </div>
--- components/ui/TailwindButton.tsx
+++ components/ui/TailwindButton.tsx
@@ -4,7 +4,6 @@ const TailwindButton = ({
   title,
   icon,
   position,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   handleClick,
   otherClasses,
 }: {
@@ -15,7 +14,10 @@ const TailwindButton = ({
   otherClasses?: string;
 }) => {
   return (
-    <button className="w-full relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none focus:ring-2 md:w-60 mt-10 md:mt-5">
+    <button
+      className="w-full relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] focus:outline-none focus:ring-2 md:w-60 mt-10 md:mt-5"
+      onClick={handleClick}
+    >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
       <span
         className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-3 ${otherClasses}`}
