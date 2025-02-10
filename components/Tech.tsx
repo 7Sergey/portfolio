71aca69da55f7ac639a073858ec9c9f91858380c
Sergey Fedorov <sergey14fedorov@gmail.com>
2025-02-10 19:02:39 +0300

    Add Floating in Tech

--- components/Tech.tsx
+++ components/Tech.tsx
@@ -1,27 +1,29 @@
 "use client";
 
 import React from "react";
-
 import { technologies } from "@/data";
+import { FloatingDock } from "./ui/Floating";
 
 const Tech = () => {
+  const techItems = technologies.map((tech) => ({
+    title: tech.name,
+    icon: <img src={tech.icon} alt={tech.name} className="md:w-10 w-5" />,
+    href: "#", // Можете заменить на реальные ссылки или убрать, если не нужно
+  }));
+
   return (
     <section id="tech" className="py-20">
       <h1 className="heading">
-        My
-        <span className="text-purple"> tech stack</span>
+        My <span className="text-purple">tech stack</span>
       </h1>
 
       <div className="flex flex-col items-center max-lg:mt-10 mt-10">
-        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
-          {technologies.map((tech) => (
-            <React.Fragment key={tech.name}>
-              <div className="flex md:max-w-60 max-w-32 gap-2">
-                <img src={tech.icon} alt={tech.name} className="md:w-10 w-5" />
-              </div>
-            </React.Fragment>
-          ))}
-        </div>
+        {/* Используем FloatingDock для отображения технологий без фона */}
+        <FloatingDock
+          items={techItems}
+          desktopClassName="bg-transparent"
+          mobileClassName="bg-transparent"
+        />
       </div>
     </section>
   );
